c2f6cfd3a57c354cad83a7367f2b2700a963b2e4
Brandon Young <67305844+byvfx@users.noreply.github.com>
2023-12-10 00:51:28 +0000

    Refactor scraping logic in houAPIspider.py

--- houAPIscraper/houAPIscraper/spiders/houAPIspider.py
+++ houAPIscraper/houAPIscraper/spiders/houAPIspider.py
@@ -1,5 +1,4 @@
 import scrapy
-import logging
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
@@ -9,20 +8,17 @@ class HouAPIspider(CrawlSpider):
     start_urls = ["https://www.sidefx.com/docs/houdini/hom/hou/index.html"]
 
     rules = (
-        Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/')), callback='parse_item'),
+        Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou')), callback='parse_item'),
 
     )
 
     def parse_item(self, response):
-    # Extract the category text and strip whitespace
-        logging.info(f"Scraping URL: {response.url}")
-        category_text = response.css('h2.label.heading.pull.left::text').get().strip()
-
-        if category_text:
-            category_text = category_text.strip()
-            yield {'category': category_text}
-        else:
-            logging.info(f"No category found for URL: {response.url}")
+        
+        yield {
+            #'category': response.css('h2.label.heading.pull.left::attr(data-title)').get(),
+             'homclass': response.css('a.label-text.homclass::text').get(),
+            # 'summary': response.css('p.summary::text').getall(),
+        }
 
     
             
