529bc31389e6f1df7062ec844db20be797812c8f
Brandon Young <67305844+byvfx@users.noreply.github.com>
2023-12-10 04:33:01 +0000

    Add custom_settings and additional start_urls to HouAPIspider

--- houAPIscraper/houAPIscraper/spiders/houAPIspider.py
+++ houAPIscraper/houAPIscraper/spiders/houAPIspider.py
@@ -4,23 +4,30 @@ from scrapy.linkextractors import LinkExtractor
 
 class HouAPIspider(CrawlSpider):
     name = "houAPI"
+    custom_settings = {'CLOSESPIDER_TIMEOUT': 60}
     allowed_domains = ["sidefx.com"]
-    start_urls = ["https://www.sidefx.com/docs/houdini/hom/hou/index.html"]
+    start_urls = ["https://www.sidefx.com/docs/houdini/hom/hou/index.html",
+                  "https://www.sidefx.com/docs/houdini/hom/hou/BaseKeyframe.html"]
 
-    rules = (Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/')), callback='parse_item',follow=True),)
+    # rules = (
+    #     Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/')), callback='parse_start_url',follow=True),
+    #     Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/BaseKeyframe.html')), callback='parse_item'),
+    #     )
 
     def parse_start_url(self, response):
         return self.parse_item(response)
     
     def parse_item(self, response):
+        print(f"Processing URL: {response.url}")  # Print the current URL being processed
         data = {
-            'title': 'hou package', 
+            'title': 'hou package',
             'categories': []
         }
 
         categories = response.xpath('//section[contains(@class, "heading pull left")]')
         for category in categories:
             category_name = category.xpath('./h2[@class="label heading pull left"]/@data-title').get()
+            print(f"Category Name: {category_name}")  # Print the category name
             category_data = {
                 'name': category_name,
                 'items': []
@@ -30,6 +37,8 @@ class HouAPIspider(CrawlSpider):
             for item in items:
                 item_title = item.xpath('./p[@class="label"]/a/text()').get()
                 item_summary = item.xpath('./p[@class="summary"]/text()').get()
+                print(f"Item Title: {item_title}")  # Print the item title
+                print(f"Item Summary: {item_summary}")  # Print the item summary
 
                 item_data = {
                     'title': item_title,
@@ -37,13 +46,13 @@ class HouAPIspider(CrawlSpider):
                     'functions': []
                 }
 
-                # Extract the methods within this category item
-                function_selectors = item.xpath('.//div[contains(@class, "collapsible method item")]')
-                for function_selector in function_selectors:
-                    function_name = function_selector.xpath('./@data-title').get()
-                    function_description = function_selector.xpath('./following-sibling::div[contains(@class, "content")]/p/text()').get()
-                    code_parts = function_selector.xpath('./following-sibling::div[contains(@class, "code-container")]//span/text()').extract()
-                    code_text = ''.join(code_parts)
+                # Extract function names
+                function_names = response.css('div.collapsible.method.item.collapsed::attr(data-title)').getall()
+
+                for function_name in function_names:
+                    # Placeholder for function description and code
+                    function_description = response.xpath('//*[@id="brief"]/div/p/text()').get()  # fix this.
+                    code_text = ''  # Adjust this to extract the correct code
 
                     function_data = {
                         'function_name': function_name,
@@ -52,7 +61,7 @@ class HouAPIspider(CrawlSpider):
                     }
                     item_data['functions'].append(function_data)
 
-                category_data['items'].append(item_data)
+                    category_data['items'].append(item_data)
 
             data['categories'].append(category_data)
 
