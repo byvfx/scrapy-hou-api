f74026e080266422d4247e8793b42369de2c1339
Brandon Young <67305844+byvfx@users.noreply.github.com>
2023-12-10 00:28:35 +0000

    Update houAPIspider.py to extract category text from response

--- houAPIscraper/houAPIscraper/spiders/houAPIspider.py
+++ houAPIscraper/houAPIscraper/spiders/houAPIspider.py
@@ -1,18 +1,28 @@
 import scrapy
+import logging
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 class HouAPIspider(CrawlSpider):
     name = "houAPI"
     allowed_domains = ["sidefx.com"]
-    start_urls = ["https://www.sidefx.com/docs/houdini/hom/hou/"]
+    start_urls = ["https://www.sidefx.com/docs/houdini/hom/hou/index.html"]
 
     rules = (
-        Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/')), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/')), callback='parse_item'),
 
     )
 
     def parse_item(self, response):
-        pass
+    # Extract the category text and strip whitespace
+        logging.info(f"Scraping URL: {response.url}")
+        category_text = response.css('h2.label.heading.pull.left::text').get().strip()
+
+        if category_text:
+            category_text = category_text.strip()
+            yield {'category': category_text}
+        else:
+            logging.info(f"No category found for URL: {response.url}")
+
     
             
