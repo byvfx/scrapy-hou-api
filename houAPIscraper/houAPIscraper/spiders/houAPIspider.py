ef491c7bee0737f3371564c2ed314c04e8d9618d
Brandon Young <67305844+byvfx@users.noreply.github.com>
2023-12-11 07:48:27 +0000

    Update houAPIspider.py with new custom settings and modified rules, got the functions kind of working

--- houAPIscraper/houAPIscraper/spiders/houAPIspider.py
+++ houAPIscraper/houAPIscraper/spiders/houAPIspider.py
@@ -5,15 +5,14 @@ from scrapy.linkextractors import LinkExtractor
 
 class HouAPIspider(CrawlSpider):
     name = "houAPI"
-    # custom_settings = {'CLOSESPIDER_TIMEOUT': 60}
+    custom_settings = {'CLOSESPIDER_TIMEOUT': 30}
     allowed_domains = ["sidefx.com"]
-    start_urls = ["https://www.sidefx.com/docs/houdini/hom/hou/index.html",
-                  "https://www.sidefx.com/docs/houdini/hom/hou/BaseKeyframe.html"]
+    start_urls = ["https://www.sidefx.com/docs/houdini/hom/hou/index.html"]
 
-    # rules = (
-    #     Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/')), callback='parse_start_url',follow=True),
-    #     Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/BaseKeyframe.html')), callback='parse_item'),
-    #     )
+    rules = (
+        Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/')), callback='parse_start_url'),
+        Rule(LinkExtractor(allow=(r'/docs/houdini/hom/hou/')), callback='parse_detail_page', follow=True),
+    )
 
     def parse_start_url(self, response):
         return self.parse_item(response)
@@ -28,9 +27,8 @@ class HouAPIspider(CrawlSpider):
         categories = response.xpath('//section[contains(@class, "heading pull left")]')
         for category in categories:
             category_name = category.xpath('./h2[@class="label heading pull left"]/@data-title').get()
-            print(f"Category Name: {category_name}")
             category_data = {
-                'name': category_name,
+                'category': category_name,
                 'items': []
             }
 
@@ -38,35 +36,27 @@ class HouAPIspider(CrawlSpider):
             for item in items:
                 item_title = item.xpath('./p[@class="label"]/a/text()').get()
                 item_summary = item.xpath('./p[@class="summary"]/text()').get()
+                detail_page_url = item.xpath('./p[@class="label"]/a/@href').get()
 
-                item_data = {
-                    'title': item_title,
-                    'summary': item_summary,
-                    'functions': []
-                }
-
-                # Extract the methods within this category item
-                function_selectors = item.xpath('.//div[contains(@class, "collapsible method item collapsed")]')
-                for function_selector in function_selectors:
-                    function_name = function_selector.xpath('./@data-title').get()
-                    function_description_parts = function_selector.xpath('./following-sibling::div[contains(@class, "content")]/p//text()').getall()
-                    function_description = ' '.join([desc.strip() for desc in function_description_parts]).replace('\n', ' ')
-                    function_description = re.sub(' +', ' ', function_description)
-
-                    # Extract code examples if any
-                    code_parts = function_selector.xpath('./following-sibling::div[contains(@class, "code-container")]//span/text()').getall()
-                    code_text = ''.join(code_parts).replace('\n', ' ')
-                    code_text = re.sub(' +', ' ', code_text)
-
-                    function_data = {
-                        'function_name': function_name,
-                        'function_description': function_description,
-                        'code_example': code_text,
-                    }
-                    item_data['functions'].append(function_data)
-
-                category_data['items'].append(item_data)
+                if detail_page_url:
+                    yield response.follow(detail_page_url, callback=self.parse_detail_page, meta={'item_title': item_title, 'item_summary': item_summary, 'category_data': category_data})
 
             data['categories'].append(category_data)
 
         yield data
+
+    def parse_detail_page(self, response):
+        item_title = response.meta['item_title']
+        item_summary = response.meta['item_summary']
+        category_data = response.meta['category_data']
+
+        function_names = response.css('div.collapsible.method.item.collapsed::attr(data-title)').getall()
+
+        item_data = {
+            'class': item_title,
+            'summary': item_summary,
+            'functions': function_names,
+        }
+        category_data['items'].append(item_data)
+
+        # Assuming you handle the final data aggregation here or in some other way
